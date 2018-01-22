6dbb6cc19572adef180b09fb55b4d6a1ee2b3046
Hossein <hossien.mo.re@gmail.com>
2018-01-22 23:38:12 +0330

    *Faster algorithm is written; *The thickness size problem is solved

--- main.cpp
+++ main.cpp
@@ -34,6 +34,75 @@ const Scalar RED_G = Scalar(50);
 Scalar image_color;
 Scalar mask_color;
 
+int thickness;
+
+void snale(Mat &img,int x,int y){
+    mask[y][x] = mask_color[0];
+    img.at<cv::Vec3b>(y,x)[0] = image_color[2];
+    img.at<cv::Vec3b>(y,x)[1] = image_color[1];
+    img.at<cv::Vec3b>(y,x)[2] = image_color[0];
+
+    prevMask = mask.clone();
+    prevImg = img.clone();
+
+    int rows = mask.rows;
+    int cols = mask.cols;
+
+    bool flag = true;
+    int offset = 1;
+    while (flag) {
+        flag = false;
+        for(int i = (y-offset); i < (y+offset+1);i++) {
+            for(int j = (x-offset); j < (x+offset+1);j++) {
+                if (i >= 0 && i < rows - 1 && j >= 0 && j < cols - 1 && mask[i][j] == 0) {
+                    if (mask[i - 1][ j] == mask_color[0] || mask[i + 1][ j] == mask_color[0] ||
+                        mask[i][ j - 1] == mask_color[0] || mask[i][ j + 1] == mask_color[0]) {
+                        mask[i][ j] = mask_color[0];
+                        img.at<cv::Vec3b>(i,j)[0] = image_color[2];
+                        img.at<cv::Vec3b>(i,j)[1] = image_color[1];
+                        img.at<cv::Vec3b>(i,j)[2] = image_color[0];
+                        flag = true;
+                    }
+                }
+            }
+        }
+        offset++;
+    }
+}
+
+void recDraw(Mat &img,int x,int y,int &rows,int &cols){
+    if (y >= 0 && y < rows - 1 && x >= 0 && x < cols - 1 && mask[y][x] == 0){
+
+        mask[y][x] = mask_color[0];
+        img.at<cv::Vec3b>(y,x)[0] = image_color[2];
+        img.at<cv::Vec3b>(y,x)[1] = image_color[1];
+        img.at<cv::Vec3b>(y,x)[2] = image_color[0];
+
+        stack<Point> poitns;
+        poitns.push(Point(x-1,y));
+        poitns.push(Point(x,y-1));
+        poitns.push(Point(x+1,y));
+        poitns.push(Point(x,y+1));
+
+        while (!poitns.empty()){
+            Point point = poitns.top();
+            poitns.pop();
+            if (point.y >= 0 && point.y < rows - 1 && point.x >= 0 && point.x < cols - 1 && mask[point.y][point.x] == 0){
+                mask[point.y][point.x] = mask_color[0];
+                img.at<cv::Vec3b>(point.y,point.x)[0] = image_color[2];
+                img.at<cv::Vec3b>(point.y,point.x)[1] = image_color[1];
+                img.at<cv::Vec3b>(point.y,point.x)[2] = image_color[0];
+
+                poitns.push(Point(point.x-1,point.y));
+                poitns.push(Point(point.x,point.y-1));
+                poitns.push(Point(point.x+1,point.y));
+                poitns.push(Point(point.x,point.y+1));
+            }
+        }
+    }
+
+}
+
 void mouseListener(int event, int x, int y, int flags, void*userdata){
     Mat &img = *((Mat*)(userdata)); // 1st cast it back, then deref
     if(event == EVENT_LBUTTONDOWN){
@@ -47,7 +116,7 @@ void mouseListener(int event, int x, int y, int flags, void*userdata){
     if(event == EVENT_MOUSEMOVE){
         if(drawing) {
             pt = Point(x, y);
-            line(img, pt, prevPt, Scalar(0, 255, 0), 2);
+            line(img, pt, prevPt, Scalar(0, 255, 0), thickness);
             line(mask,pt,prevPt,Scalar(255));
             prevPt = pt;
         }
@@ -55,47 +124,25 @@ void mouseListener(int event, int x, int y, int flags, void*userdata){
     if(event == EVENT_LBUTTONUP){
         if(drawing) {
             pt = Point(x, y);
-            line(img, pt, prevPt, Scalar(0, 255, 0), 2);
+            line(img, pt, prevPt, Scalar(0, 255, 0), thickness);
+            line(mask,pt,prevPt,Scalar(255));
             drawing = false;
         }
     }
 
     if(event == EVENT_RBUTTONDOWN){
-
         prevMasks.push(mask.clone());
         prevImgs.push(img.clone());
-
-        mask[y][x] = mask_color[0];
-        img.at<cv::Vec3b>(y,x)[0] = image_color[2];
-        img.at<cv::Vec3b>(y,x)[1] = image_color[1];
-        img.at<cv::Vec3b>(y,x)[2] = image_color[0];
-
-        prevMask = mask.clone();
-        prevImg = img.clone();
-
-        int rows = mask.rows;
-        int cols = mask.cols;
-
-        bool flag = true;
-        int offset = 1;
-        while (flag) {
-            flag = false;
-            for(int i = (y-offset); i < (y+offset+1);i++) {
-                for(int j = (x-offset); j < (x+offset+1);j++) {
-                    if (i >= 0 && i < rows - 1 && j >= 0 && j < cols - 1 && mask[i][j] == 0) {
-                        if (mask[i - 1][ j] == mask_color[0] || mask[i + 1][ j] == mask_color[0] ||
-                                mask[i][ j - 1] == mask_color[0] || mask[i][ j + 1] == mask_color[0]) {
-                            mask[i][ j] = mask_color[0];
-                            img.at<cv::Vec3b>(i,j)[0] = image_color[2];
-                            img.at<cv::Vec3b>(i,j)[1] = image_color[1];
-                            img.at<cv::Vec3b>(i,j)[2] = image_color[0];
-                            flag = true;
-                        }
-                    }
-                }
-            }
-            offset++;
+        try {
+            int rows = mask.rows;
+            int cols = mask.cols;
+            //snale(img, x,y);
+            recDraw(img,x,y,rows,cols);
         }
+        catch (Exception e){
+            cout << e.err;
+        }
+
     }
 }
 
@@ -123,6 +170,15 @@ int main(int argc, char** argv )
 
     // initialize mask
     mask = Mat_<int>::zeros(image.rows,image.cols);
+
+    int num = image.rows;
+    int dnum = 720;
+
+    if(num < image.cols) {
+        num = image.cols;
+        dnum = 640;
+    }
+    thickness = (int)(num/dnum)*2;
 
     namedWindow(window, WINDOW_NORMAL );
     resizeWindow(window,720,640);
